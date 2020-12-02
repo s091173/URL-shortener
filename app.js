22c72d53c1ba39486f606ec5b339f4b860a40cbf
yuwen chang <provence5058@gmail.com>
2020-12-02 16:12:46 +0800

    feat: add body-parser & index page

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 
 app.listen(() => {
