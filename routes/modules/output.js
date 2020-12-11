7710c3afca263f86d5ae41203383305f23c5e475
yuwen chang <provence5058@gmail.com>
2020-12-11 20:34:49 +0800

    feat: modify home page & output.js

--- routes/modules/output.js
+++ routes/modules/output.js
@@ -4,6 +4,9 @@ const URL = require('../../models/url')
 const bodyParser = require('body-parser')
 const generateShortenedURL = require('../../URLshorten.js')
 router.use(bodyParser.urlencoded({ extended: true }))
+
+
+
 // 輸入網址，按下shorten後
 router.post('/output', (req, res) => {
   let shortened = '' // 將縮短的網址部分設為變數shortened
@@ -11,18 +14,25 @@ router.post('/output', (req, res) => {
   URL.find({ link }) // 先看看資料庫內是否已有相對應的短網址
     .lean()
     .then(links => {
-      if (links.length !== 0) { // 如果有
+      if (links.length !== 0) { // 如果有已有相對應的短網址
         shortened = links[0].shortenedURL
-      } else { // 如果沒有
+      } else { // 如果沒有相對應的短網址
         shortened = generateShortenedURL() // 儲存剛做完的短網址
-        URL.create({  // 寫入資料庫
-          link,  // 新增新的網頁連結
-          shortenedURL: shortened  // 及新增相對應的短網址
-        })
+        URL.find({ shortenedURL: shortened })  //確認新產生的網址與之前的網址沒有重複  
+          .lean()
+          .then(urls => {
+            while (urls.length !== 0) { //如果有重複的話就一直重新產生新短網址 並 將新的短網址覆蓋過去
+              shortened = generateShortenedURL()
+            }
+            URL.create({  // 寫入資料庫
+              link,  // 新增新的網頁連結
+              shortenedURL: shortened  // 及新增相對應的短網址
+            })
+          })
       }
     })
     .then(() => {
-      console.log(link, shortened)
+      // console.log(link, shortened)
       res.render('output', { link, shortened })   // 點擊連結可導向  user 想進入的頁面
     }).catch(error => console.log(error))
 
@@ -30,7 +40,6 @@ router.post('/output', (req, res) => {
 
 router.get(`/:shortened`, (req, res) => {
   const url = req.params.shortened
-  console.log(url)
   URL.find({ shortenedURL: url })
     .lean()
     .then(urls => {
