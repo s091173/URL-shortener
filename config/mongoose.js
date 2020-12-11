3293b816f80e073203e80bb301b69ae59d112bc3
yuwen chang <provence5058@gmail.com>
2020-12-11 17:32:27 +0800

    feat: modify mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/url-list'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log(err))
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
