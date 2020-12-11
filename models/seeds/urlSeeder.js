7b1dbb78f5611af4c08d7cd2ab51bf384c3d796c
yuwen chang <provence5058@gmail.com>
2020-12-11 16:43:01 +0800

    feat: modify urlSeeder

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,5 +1,19 @@
 const URL = require('../url')
 const db = require('../../config/mongoose')
 db.once('open', () => {
-  console.log('urlSeeder has done!')
+  console.log('db connected!')
+  URL.create({
+    link: 'https://www.google.com/',
+    shortenedURL: 'B4fUv'
+  }, {
+    link: 'https://www.youtube.com',
+    shortenedURL: 'SaSKE'
+  }, {
+    link: 'https://mongoosejs.com',
+    shortenedURL: 'zif1r'
+  })
+    .then(() => {
+      console.log('urlSeeder has done!')
+      db.close()
+    })
 })
